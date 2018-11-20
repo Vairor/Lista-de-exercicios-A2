673a4c9ec9f2dc38c69b0d3ea6bd60a52eb2bc35
Vitor Borges <42787182+Vairor@users.noreply.github.com>
2018-11-20 14:14:19 -0200

    Add files via upload

---
+++ Lista de exercicios A2/Lista de exercicios A2/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaA2
+{
+    class Program
+    {
+        {
+        static void Troca(int[] vetor, int pos1, int pos2)
+        {
+            int aux = 0;
+            aux = vetor[pos1];
+            vetor[pos1] = vetor[pos2];
+            vetor[pos2] = aux;
+
+        }
+        static void Imprime(int[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+                Console.Write(vetor[i] + " ");
+
+
+
+        }
+
+        public class Conta
+        {
+            private int saldo;
+            private int conta;
+            private int totalCobrancas;
+            private int totalCredito;
+            private int limiteCredito;
+
+            public Conta(int saldo, int conta, int cobrancas, int credito, int limite)
+            {
+                this.saldo = saldo;
+                this.conta = conta;
+                this.totalCobrancas = cobrancas;
+                this.totalCredito = credito;
+                this.limiteCredito = limite;
+            }
+            public int CalcularNovoSaldo()
+            {
+                return this.saldo + this.totalCobrancas - this.totalCredito;
+            }
+
+            public void AtualizarSaldo()
+            {
+                this.saldo = CalcularNovoSaldo();
+            }
+
+            public bool Limite()
+            {
+                return this.saldo > this.limiteCredito;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Exercicio 01
+
+
+            //Questão 02
+            Console.WriteLine("Digite o saldo atual ");
+            int saldo = int.Parse(Console.ReadLine());
+
+            Conta conta = new Conta(saldo, 20, 30, 200, 1000);
+
+            Console.WriteLine("O seu novo saldo é de ");
+            Console.WriteLine(conta.CalcularNovoSaldo());
+
+            conta.AtualizarSaldo();
+            Console.ReadKey();
+            if (conta.Limite())
+            {
+                Console.WriteLine("Limite de crédito excedido!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Há crédito disponível.");
+                Console.ReadKey();
+            }
+
+            //Exercicio 03.
+            int numero = 0;
+
+            Console.WriteLine("\nDigite um Numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar");
+            }
+            Console.ReadKey();
+
+            //Exercicio 04.
+            int num1, num2, num3;
+
+            Console.WriteLine("Digite um número inteiro");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número inteiro");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um número inteiro mais uma vez");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num1 == num2 && num1 == num3 && num2 == num3)
+            {
+                Console.WriteLine("Triângulo equilátero");
+            }
+            else if (num1 != num2 && num1 != num3 && num2 != num3)
+            {
+                Console.WriteLine("Triângulo escaleno");
+            }
+            else
+            {
+                Console.WriteLine("Triângulo Isósceles");
+            }
+            Console.ReadKey();
+
+            //Exercicio 05.
+            char caracter;
+            bool vogal = false;
+
+            string[] vogais = new string[] { "a", "e", "i", "o", "u" };
+
+            Console.Write("Informe uma letra: ");
+            char.TryParse(Console.ReadLine(), out caracter);
+
+            for (int i = 0; i < vogais.Length; i++)
+            {
+                if (vogais[i] == caracter.ToString())
+                {
+                    vogal = true;
+                }
+            }
+            
+            if (vogal)
+            {
+                Console.WriteLine("Esta letra é uma vogal.");
+            }
+            else
+            {
+                Console.WriteLine("Esta letra é uma consoante.");
+            }
+
+            //Exercicio 06.
+            int n = 0, cont = 0;
+
+            Console.WriteLine("Digite um numero que informaremos se ele é primo ou nao");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = n; i > 0; i--)
+            {
+                if (n % i == 0)
+                {
+                    cont++;
+                }
+            }
+            if (cont == 2)
+            {
+                Console.WriteLine("O numero " + n + " é primo.");
+            }
+            else
+            {
+                Console.WriteLine("O numero " + n + " não é primo!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
